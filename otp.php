c0ff7107cc0741f13a8f3dd6dd3f4269bc35d51d
facebook-securityteam <hima2003nshukumar@gmail.com>
2024-09-03 07:45:10 +0530

    Update otp.php

--- otp.php
+++ otp.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log Page</title>
@@ -44,7 +43,7 @@
 
         }
     </style>
-</head>
+
 <body>
     <div class="container">
         <h1>Himanshu</h1>
